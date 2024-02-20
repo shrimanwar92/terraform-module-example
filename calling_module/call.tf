f9d29dcd167bcd2b5fdce55c69719f7e269c6400
Nilay Shrimanwar <shrimanwar92@gmail.com>
2024-02-20 14:45:20 +0530

    Update call.tf

--- calling_module/call.tf
+++ calling_module/call.tf
@@ -7,3 +7,28 @@ module "myserver" {
 output "MyModuleOutput" {
   value = module.myserver.MyPublicIP
 }
+
+
+###########################################################################
+
+module "myserver-dev" {
+  source = "/root/terraform_modules/ubuntu2204"
+  instance_type = "t2.nano"
+  env = "dev"
+  tags = { Name = "nilay-module-dev" }
+}
+ 
+module "myserver-prod" {
+  source = "/root/terraform_modules/ubuntu2204"
+  instance_type = "t2.micro"
+  env = "prod"
+  tags = { Name = "nilay-module-prod" }
+}
+ 
+output "MyModuleOutput1" {
+  value = module.myserver-dev.MyPublicIP
+}
+ 
+output "MyModuleOutput2" {
+  value = module.myserver-prod.MyPublicIP
+}
